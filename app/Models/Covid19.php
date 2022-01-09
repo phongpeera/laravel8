3bb21c9654655761f5c29f8f68749ccfad8ba236
Phongpeera <phongpeera@gmail.com>
2022-01-09 17:10:13 +0700

    Week 6

--- app/Models/Covid19.php
+++ app/Models/Covid19.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Covid19 extends Model
 {
     use HasFactory;
+    //ชื่อตารางในฐานข้อมูล
     protected $table = "covid19s";
+    //ชื่อคอลัมน์ในฐานข้อมูลที่อนุญาติให้แก้ไขข้อมูล
+    protected $fillable = ["date", "country", "total", "active", "death", "recovered", "total_in_1m", "remark"];
+    //Primary Key
+    protected $primaryKey = "id";
 }
--- routes/web.php
+++ routes/web.php
@@ -135,4 +135,20 @@ Route::get("/newgallery/{name}", [ MyProfileController::class, "galleryByName"])
 // Week 5
 Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
 
-Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+// Week 6
+
+Route::resource('/covid19', Covid19Controller::class );
+
+// Route::get("/covid19/create",[ Covid19Controller::class , "create" ]);
+
+// Route::get("/covid19/{id}/edit", [ Covid19Controller::class , "edit" ]);
+
+// Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+
+// Route::get('/covid19/{id}',[ Covid19Controller::class,'show' ]);
+
+// Route::post("/covid19",[ Covid19Controller::class , "store" ]);
+
+// Route::patch("/covid19/{id}", [ Covid19Controller::class , "update" ]);
+
+// Route::delete('/covid19/{id}', [ Covid19Controller::class , 'destroy' ]);
