a8fe9b7722574ad9a842726d54c0fa181b2cb956
Phongpeera <phongpeera@gmail.com>
2022-01-30 16:33:39 +0700

    Week 9 : Authentication

--- routes/web copy.php
+++ routes/web copy.php
@@ -23,12 +23,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
-
 Route::get('/homepage', function () {
     return "<h1>This is home page</h1>";
 });
@@ -75,38 +69,38 @@ Route::get("/gallery", function () {
     $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
     $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
 
-    return view("test/index", compact("name", "ant", "bird", "cat", "god", "spider"));
+    return view("test/index", compact("name","ant", "bird", "cat", "god", "spider"));
 });
 
 Route::get("/gallery/ant", function () {
     $name = "Phongpeera 63222390001";
     $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    return view("test/ant", compact("name", "ant"));
+    return view("test/ant", compact("name","ant"));
 });
 
 Route::get("/gallery/bird", function () {
     $name = "Phongpeera 63222390001";
     $bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
-    return view("test/bird", compact("name", "bird"));
+    return view("test/bird", compact("name","bird"));
 });
 
 Route::get("/gallery/cat", function () {
     $name = "Phongpeera 63222390001";
     $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    return view("test/cat", compact("name", "cat"));
+    return view("test/cat", compact("name","cat"));
 });
 
 // WEEK 3
-Route::get("/teacher", function () {
-    return view("teacher");
-})->middleware('auth', 'role:admin,teacher');
+Route::get("/teacher" , function (){
+	return view("teacher");
+});
 
-Route::get("/student", function () {
-    return view("student");
-})->middleware('auth', 'role:admin,teacher');
+Route::get("/student" , function (){
+	return view("student");
+});
 
-Route::get("/theme", function () {
-    return view("theme");
+Route::get("/theme" , function (){
+	return view("theme");
 });
 
 // Route Template Inheritance
@@ -131,23 +125,23 @@ Route::get('/table', function () {
 });
 
 // Week 4
-Route::get("/myprofile/create", [MyProfileController::class, "create"]);
+Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
 
-Route::get("/myprofile/{id}/edit", [MyProfileController::class, "edit"]);
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
 
-Route::get("/myprofile/{id}", [MyProfileController::class, "show"]);
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
 
 
-Route::get("/newgallery", [MyProfileController::class, "gallery"]);
+Route::get("/newgallery", [ MyProfileController::class, "gallery"]);
 
-Route::get("/newgallery/{name}", [MyProfileController::class, "galleryByName"]);
+Route::get("/newgallery/{name}", [ MyProfileController::class, "galleryByName"]);
 
 // Week 5
-Route::get("/coronavirus", [MyProfileController::class, "coronavirus"]);
+Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
 
 // Week 6
 
-Route::resource('/covid19', Covid19Controller::class);
+Route::resource('/covid19', Covid19Controller::class );
 
 // Route::get("/covid19/create",[ Covid19Controller::class , "create" ]);
 
@@ -164,7 +158,7 @@ Route::resource('/covid19', Covid19Controller::class);
 // Route::delete('/covid19/{id}', [ Covid19Controller::class , 'destroy' ]);
 
 // Week 7 Exercise CRUD
-Route::resource('/staff', StaffController::class);
+Route::resource('/staff', StaffController::class );
 
 // Week 7 CRUD Generator
 // Route::resource('post', 'PostController');
